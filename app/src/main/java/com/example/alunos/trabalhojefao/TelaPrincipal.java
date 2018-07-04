8d765829ce01c3bbec6169bdc70b0e244454065e
gab1280gh <gab1280gh@gmail.com>
2018-07-04 19:48:13 -0300

    Telineas vazineas

--- app/src/main/java/com/example/alunos/trabalhojefao/TelaPrincipal.java
+++ app/src/main/java/com/example/alunos/trabalhojefao/TelaPrincipal.java
@@ -1,13 +1,43 @@
 package com.example.alunos.trabalhojefao;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class TelaPrincipal extends AppCompatActivity {
+public class TelaPrincipal extends AppCompatActivity implements View.OnClickListener {
+
+    Button bt_clientes, bt_produtos, bt_pedidos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_principal);
+
+        bt_clientes = findViewById(R.id.BT_clientes);
+        bt_produtos = findViewById(R.id.BT_produtos);
+        bt_pedidos = findViewById(R.id.BT_pedidos);
+
+        bt_clientes.setOnClickListener(this);
+        bt_produtos.setOnClickListener(this);
+        bt_pedidos.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (bt_clientes.getId() == v.getId()){
+            Intent it = new Intent (TelaPrincipal.this, TelaClientes.class);
+            startActivity(it);
+        }
+        if (bt_produtos.getId() == v.getId()){
+            Intent it = new Intent (TelaPrincipal.this, TelaProdutos.class);
+            startActivity(it);
+        }
+        if (bt_pedidos.getId() == v.getId()){
+            Intent it = new Intent (TelaPrincipal.this, TelaPedidos.class);
+            startActivity(it);
+        }
     }
 }
