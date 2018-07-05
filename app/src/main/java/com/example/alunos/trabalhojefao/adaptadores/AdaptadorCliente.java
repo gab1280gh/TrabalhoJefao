4d888e6fdd202632f3573598763993e6d585b1f9
gab1280gh <gab1280gh@gmail.com>
2018-07-04 21:02:41 -0300

    Manipula cliente

---
+++ app/src/main/java/com/example/alunos/trabalhojefao/adaptadores/AdaptadorCliente.java
@@ -0,0 +1,4 @@
+package com.example.alunos.trabalhojefao.adaptadores;
+
+public class AdaptadorCliente {
+}
--- app/src/main/java/com/example/alunos/trabalhojefao/banco/Cliente.java
+++ app/src/main/java/com/example/alunos/trabalhojefao/banco/Cliente.java
@@ -6,6 +6,8 @@ public class Cliente {
 
     private String cpf;
 
+    private String nome;
+
     private String telefone;
 
     private String endereco;
@@ -25,6 +27,10 @@ public class Cliente {
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
+
+    public String getNome() {return nome;}
+
+    public void setNome(String nome) {this.nome = nome;}
 
     public String getTelefone() {
         return telefone;
